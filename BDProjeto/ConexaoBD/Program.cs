bb953f507b80a86b44a95d94e44dc26f03db9c99
LucasBarga <lucasmpbarga@gmail.com>
2018-01-18 23:15:09 -0200

    aula22

---
+++ BDProjeto/ConexaoBD/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var bd = new bd();
+            var usuarioAplicacao = new UsuarioAplicacao();
+            
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Cargo: ");
+            string cargo = Console.ReadLine();
+            Console.Write("Data: ");
+            string date = Console.ReadLine();
+            
+            var usuarios = new Usuarios
+            {
+                Nome = nome,
+                Cargo = cargo,
+                Date = DateTime.Parse(date)
+            };
+            usuarios.Id = 1;
+            usuarioAplicacao.Update(usuarios);
+            usuarioAplicacao.Insert(usuarios);
+
+            string strQuerySelect = "SELECT * FROM usuarios ORDER BY usuarioId";
+            SqlDataReader dados = bd.ExecutaComandoComRetorno(strQuerySelect);
+            while (dados.Read())
+            {
+                Console.WriteLine("Id:{0} Nome:{1} Cargo:{2} Data:{3}", dados["usuarioId"], dados["nome"], dados["cargo"], dados["date"]);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ BDProjeto/ConexaoBD/UsuarioAplicacao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoBD
+{
+    class UsuarioAplicacao
+    {
+        private bd bd;
+
+        public void Insert(Usuarios usuarios)
+        {
+            var strQuery = "";
+            strQuery += "INSERT INTO usuarios(nome, cargo, date)";
+            strQuery += string.Format(" VALUES ('{0}','{1}','{2}')", usuarios.Nome, usuarios.Cargo, usuarios.Date
+                );
+            using (bd = new bd())
+            {
+                bd.ExecutaComando(strQuery);
+            }
+        }
+
+        public void Update(Usuarios usuarios)
+        {
+            var strQuery = "";
+            strQuery += "UPDATE usuarios SET ";
+            strQuery += string.Format("nome = '{0}',", usuarios.Nome);
+            strQuery += string.Format("cargo = '{0}',", usuarios.Cargo);
+            strQuery += string.Format("date = '{0}'", usuarios.Date);
+            strQuery += string.Format(" WHERE usuarioId = '{0}'", usuarios.Id);
+
+            using (bd = new bd())
+            {
+                bd.ExecutaComando(strQuery);
+            }
+        }
+
+        //CONTINUAR_AULA22
+        public void Salvar()
+        {
+
+        }
+
+
+    }
+}
